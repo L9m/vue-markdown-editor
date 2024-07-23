986299d07b6fa2827f5c985fb02b6ab049f97a29
刘东明 <liudongming@qimingdaren.com>
2024-07-23 10:09:54 +0800

    chore: release 2.4.0

--- src/preview-html.js
+++ src/preview-html.js
@@ -3,7 +3,7 @@ import Component from './preview-html.vue';
 // font css
 import '@/assets/css/font';
 
-const version = '1.7.12';
+const version = '2.4.0';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);

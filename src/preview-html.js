8f79b971111fc1e056442a0509f0f90f066a865a
刘东明 <liudongming@qimingdaren.com>
2024-07-29 17:40:31 +0800

    chore: release 2.4.5

--- src/preview-html.js
+++ src/preview-html.js
@@ -3,7 +3,7 @@ import Component from './preview-html.vue';
 // font css
 import '@/assets/css/font';
 
-const version = '2.4.4';
+const version = '2.4.5';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);
--- src/preview.js
+++ src/preview.js
@@ -5,7 +5,7 @@ import Lang from '@/lang/';
 // font css
 import '@/assets/css/font';
 
-const version = '2.4.4';
+const version = '2.4.5';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);

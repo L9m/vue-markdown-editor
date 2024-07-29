b94e82cb59ffdf49b12ad077220d6a2d03a1779c
刘东明 <liudongming@qimingdaren.com>
2024-07-29 17:33:55 +0800

    chore: release 3.1.7

--- src/codemirror-editor.js
+++ src/codemirror-editor.js
@@ -4,7 +4,7 @@ import xss from '@/utils/xss/index';
 // font css
 import '@/assets/css/font';
 
-const version = '3.1.6';
+const version = '3.1.7';
 
 const install = (app) => {
   app.component(Component.name, Component);

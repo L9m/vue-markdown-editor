d73e66e0a3c11281f0925beec07ea872cc394f12
刘东明 <liudongming@qimingdaren.com>
2024-07-30 10:25:09 +0800

    chore: release 2.4.7

--- src/codemirror-editor.js
+++ src/codemirror-editor.js
@@ -5,7 +5,7 @@ import Lang from '@/lang/';
 // font css
 import '@/assets/css/font';
 
-const version = '2.4.6';
+const version = '2.4.7';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);
--- src/preview-html.js
+++ src/preview-html.js
@@ -3,7 +3,7 @@ import Component from './preview-html.vue';
 // font css
 import '@/assets/css/font';
 
-const version = '2.4.6';
+const version = '2.4.7';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);

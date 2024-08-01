a7de791dbb3cbf5c6b3146c53caa36f5c9f29567
刘东明 <liudongming@qimingdaren.com>
2024-08-01 11:07:28 +0800

    chore: release 3.1.13

--- src/preview.js
+++ src/preview.js
@@ -4,7 +4,7 @@ import xss from '@/utils/xss/index';
 // font css
 import '@/assets/css/font';
 
-const version = '3.1.12';
+const version = '3.1.13';
 
 const install = (app) => {
   app.component(Component.name, Component);

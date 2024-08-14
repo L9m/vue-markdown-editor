aae2075b62608b386f6c1601cf327a78e681ef18
刘东明 <liudongming@qimingdaren.com>
2024-08-14 16:48:23 +0800

    chore: release 2.4.15

--- src/codemirror-editor.js
+++ src/codemirror-editor.js
@@ -5,7 +5,7 @@ import Lang from '@/lang/';
 // font css
 import '@/assets/css/font';
 
-const version = '2.4.14';
+const version = '2.4.15';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);

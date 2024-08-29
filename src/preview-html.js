999aee6e3a460c08426e9d279433d7baf89f3282
刘东明 <liudongming@qimingdaren.com>
2024-08-29 08:54:45 +0800

    chore: release 3.1.28-beta.0

--- src/preview-html.js
+++ src/preview-html.js
@@ -3,7 +3,7 @@ import Component from './preview-html.vue';
 // font css
 import '@/assets/css/font';
 
-const version = '3.1.27';
+const version = '3.1.28-beta.0';
 
 const install = (app) => {
   app.component(Component.name, Component);

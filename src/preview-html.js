ede4aed378623c12a7d4ea216061c5b69d141ee4
刘东明 <liudongming@qimingdaren.com>
2024-09-04 14:26:03 +0800

    chore: release 3.2.6

--- src/preview-html.js
+++ src/preview-html.js
@@ -3,7 +3,7 @@ import Component from './preview-html.vue';
 // font css
 import '@/assets/css/font';
 
-const version = '3.2.6-beta.1';
+const version = '3.2.6';
 
 const install = (app) => {
   app.component(Component.name, Component);

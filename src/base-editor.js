2dc2438f1603011bb69eafc279fbda0474a9c92d
刘东明 <liudongming@qimingdaren.com>
2025-11-03 10:22:41 +0800

    chore: release 3.2.14

--- src/base-editor.js
+++ src/base-editor.js
@@ -4,7 +4,7 @@ import xss from '@/utils/xss/index';
 // font css
 import '@/assets/css/font';
 
-const version = '3.2.13';
+const version = '3.2.14';
 
 const install = (app) => {
   app.component(Component.name, Component);
--- src/preview-html.js
+++ src/preview-html.js
@@ -3,7 +3,7 @@ import Component from './preview-html.vue';
 // font css
 import '@/assets/css/font';
 
-const version = '3.2.13';
+const version = '3.2.14';
 
 const install = (app) => {
   app.component(Component.name, Component);
